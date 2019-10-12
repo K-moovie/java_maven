b73e0254e854aa58b638c34c84529ff0b22e6c11
kyha97 <kyha97@naver.com>
2019-10-12 17:24:00 +0900

    revise ch06

--- practice/src/main/java/cse/practice/ch06/enhancedsimpledotcom/SimpleDotCom.java
+++ practice/src/main/java/cse/practice/ch06/enhancedsimpledotcom/SimpleDotCom.java
@@ -4,21 +4,22 @@ package cse.practice.ch06.enhancedsimpledotcom;
 import java.util.ArrayList;
 
 public class SimpleDotCom {
-    private ArrayList<String> locationCells;
+    private ArrayList<String> locationCells; 
+    // 위치를 저장할 ArrayList형 자료구조 생성
     
     public void setLocationCells(ArrayList<String> loc){
         locationCells = loc;
-        System.out.println(loc.get(0));
-        System.out.println(loc.get(1));
-        System.out.println(loc.get(2));
+//        System.out.println(loc.get(0)); //test용
+//        System.out.println(loc.get(1));
+//        System.out.println(loc.get(2));
     }
     
     public String checkYourself(String userInput){
         String result = "miss";
         int index = locationCells.indexOf(userInput); // userInput이 있는 index반환 
         if(index >= 0) { // 올바른 숫자가 입력했을 때
-            locationCells.remove(index);
-            if(locationCells.isEmpty()){
+            locationCells.remove(index); // ArrayList의 해당 내용 삭제
+            if(locationCells.isEmpty()){ // ArrayList가 비었으면 kill
                 result = "kill";
             } else{
                 result = "hit";
--- practice/src/main/java/cse/practice/ch06/enhancedsimpledotcom/SimpleDotComGame.java
+++ practice/src/main/java/cse/practice/ch06/enhancedsimpledotcom/SimpleDotComGame.java
@@ -6,20 +6,21 @@ import java.util.Collections;
 
 public class SimpleDotComGame {
     public static void main(String[] args){
-        int numOfGuesses = 0;
-        GameHelper helper = new GameHelper();
+        int numOfGuesses = 0; // 게임 횟수를 세는 int형 변수
+        GameHelper helper = new GameHelper(); // GameHelper객체 생성
         
-        SimpleDotCom theDotCom = new SimpleDotCom();
-        int randomNum = (int)(Math.random()*5); //0~4까지의 int형 난수 생성
-        ArrayList<String> locations= new ArrayList<>();
+        SimpleDotCom theDotCom = new SimpleDotCom(); // SimpleDotCom객체 생성
+        int randomNum = (int)(Math.random()*5); // 0~4까지의 int형 난수 생성
+        ArrayList<String> locations= new ArrayList<>(); // randomNum을 저장할 ArrayList 생성  
         for(int i = 0; i < 3; i++)
         {
             String rand = Integer.toString(randomNum+i);
-            locations.add(rand);
+            // int형 변수를 String형으로 변환
+            locations.add(rand); // ArrayList에 추가
         }
         theDotCom.setLocationCells(locations);
         
-        boolean isAlive = true;
+        boolean isAlive = true; // 게임 종료 여부를 저장하는 변수
         while(isAlive == true)
         {
             String guess = helper.getUserInput("enter a number");
