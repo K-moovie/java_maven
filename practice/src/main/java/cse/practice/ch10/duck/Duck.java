2d913616d0b8b3a022e008a9df2f64930ad17667
KIM <kyha97@naver.com>
2019-11-01 00:41:01 +0900

    Add ch10.

---
+++ practice/src/main/java/cse/practice/ch10/duck/Duck.java
@@ -0,0 +1,46 @@
+
+package cse.practice.ch10.duck;
+
+public class Duck {
+    private int size;
+    private static int duckCount = 0;
+    private int nth;
+    private static Duck[] ducks;
+    
+    public Duck(){ // 기본 생성자 
+        this(10);
+    }
+    
+    public Duck(int size){ // 매개변수가 있는 생성자
+        this.size = size;
+        nth = duckCount++; // 인스턴스 변수 = static변수
+    }
+    
+    @Override
+    public String toString(){ 
+        StringBuilder sb = new StringBuilder(); 
+        sb.append("나는 "); // 기존데이터에 해당 문자열을 추가함
+        sb.append(nth);
+        sb.append("번째 오리이고, ");
+        sb.append("크기는 ").append(size).append("입니다."); // 자기 자신을 반환 하기 때문에 병렬로 사용 가능
+        
+        return sb.toString();
+    }
+    
+    public static void initialize(){
+        ducks = new Duck[10];
+        ducks[0] = new Duck(); // 기본 생성자 호출
+        
+        for(int i = 1; i<ducks.length;i++){
+            int size = (int)(Math.random()*20)+10;
+            ducks[i] = new Duck(size); // 매개변수가 있는 생성자 호출
+        }
+    }
+    
+    public static void main(String[] args){
+        initialize();
+        for(Duck d : ducks){
+            System.out.println(d);
+        }
+    }
+}
---
+++ practice/src/main/java/cse/practice/ch10/duck/WrapperClassTest.java
@@ -0,0 +1,63 @@
+
+package cse.practice.ch10.duck;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
+
+public class WrapperClassTest {
+    public static void doSomething(List<Integer>numbers){ // List<Integer>형 매개변수를 받는다.
+        System.out.print("첫 번째 출력: ");
+        numbers.forEach(e->System.out.print(e.toString()+"   "));
+        
+        System.out.print("\n두 번째 출력: ");
+        for (int e : numbers){
+            System.out.print(e+"   ");
+        }
+        System.out.print("\n합계1: ");
+        Optional<Integer> sum1 = numbers.stream().reduce((x,y)->x+y); // 람다표현식
+        if(sum1.isPresent()){
+            System.out.println(sum1.get()); // Optional<>의 메소드
+        }
+        
+        System.out.print("합계2: "); 
+        Integer sum2 = numbers.stream().reduce(0,Integer::sum);
+        System.out.println(sum2);
+        
+        System.out.print("합계3: ");
+        Integer sum3 = numbers.stream().mapToInt(e->e).sum(); // 중간연산자(mapToInt)필요
+        System.out.println(sum3);
+        
+        System.out.print("평균: ");
+        OptionalDouble avg = numbers.stream().mapToInt(e->e).average();
+        if(avg.isPresent()){
+            System.out.println(avg.getAsDouble());
+        } 
+    }
+    
+    public static void doConversion(){
+        Integer i1 = new Integer("123"); // 권장하지 않음
+        Integer i2 = Integer.parseInt("123"); // String형 "123"을 int형 123으로 변환 
+        String s1 = i1.toString(); // Integer를 String형으로 변환
+        int number = i2.intValue(); // 언박싱
+        
+        System.out.printf("%d %d %s %d%n",i1,i2,s1,number);
+        try{
+            Integer i3 = Integer.parseInt("123.5"); // 예외발생
+        }catch(NumberFormatException ex){
+            System.err.println("에외발생:" +ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Integer> numbers = new LinkedList<>();
+        
+        for(int i =0; i < 20; i ++){
+            int value = (int)(Math.random()*100);
+            numbers.add(value); // 오토박싱
+        }
+        doSomething(numbers);
+        doConversion();
+    }
+}
